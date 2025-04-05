34b265c22e2447e9cf98c844b4ee931a88402d13
khaledNasr56 <kn3846228@gmail.com>
2025-04-05 16:28:42 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
-import Signup from './components/Signup';
+// import './App.css';
+// import Signup from './components/Signup';
+
+import Login from "./components/Login";
 
 const App = () =>{
   
 return(
    <>
-   <Signup/>
+   <Login/>
    </>
 )
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,34 @@
-/* eslint-disable no-unused-vars */
-import React from "react"
-import { useState } from "react"
-import axios from 'axios'
 
-function Login(){
-    const [post, setPost] = useState({
-        title:'',
-        body:''
-    })
-    const handelInput =(event) =>{
-      setPost({...post, [event.target.name]: event.target.event})
-    }
-    function handleSubmit(event){
-        event.preventDefault()
-        axios.post('https://jsonplaceholder.typicode.com/posts', {
-            title: 'My Title',
-            body: 'This is a test post',
-            userId: 1
-        })
-        .then(response => {
-            console.log('Success:', response.data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-        
-    }
+const Login = () => {
+  return (
+  <section className="container forms">
+    <div className="form">
+        <div className="form-content">
+            <header>Login</header>
+         
+            <form action="">
+                <div className="input-field">
+                    <input type="email" placeholder="Enter Your Email" 
+                    className="input"/>
+                </div>
 
-        return (
-            <div style={{width:'fit-content',
-              margin:'auto', position:'absolute', top:'30%', left:'40%',}}>
-        
-            <div className="w-50 text-center" style={{display:'flex', flexDirection:'column'}}>
-        
-              <form onSubmit={handleSubmit} style={{flexDirection:"column" }} >
-                Title:<input  type="text" onChange={handelInput} name="title"/> <br />
-                post:<input type="text" onChange={handelInput} name="post"/> <br />
-        
-                <button className="btn btn-primary" style={{marginLeft:'2rem'}}>Submit</button>
-              </form>
-            </div>
-        
-            </div>
-            )
-        }
-    
-    
-    export default Login
+                <div className="input-field">
+                    <input type="password" placeholder="Enter Your password" 
+                    className="password"/>
+
+                <div className="form-link">
+                    <a href="#" className="forgot-pass">Forget Password</a>
+                </div>
+
+
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+  </section>
+  )
+}
+
+export default Login
